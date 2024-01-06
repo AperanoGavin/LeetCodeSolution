988951e4c147945489217f323f3084f33a53e3c5
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2024-01-06 16:20:33 +0100

    Time: 11 ms (24.00%) | Memory: 19.7 MB (6.67%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.php
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function invertTree($root) {
+        if($root->val == []){
+            return [];
+        }
+
+        $tmp =$root->left;
+        $root->left = $root->right;
+        $root->right = $tmp;
+        $this->invertTree($root->right);
+        $this->invertTree($root->left);
+
+        return $root;
+
+        
+    }
+}
