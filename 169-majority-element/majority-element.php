baae48470e9ef515241c8dfd6a8c4c5888e95d92
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2024-11-13 12:15:47 +0100

    Time: 0 ms (100.00%) | Memory: 23.2 MB (24.48%) - LeetSync

--- 169-majority-element/majority-element.php
+++ 169-majority-element/majority-element.php
@@ -5,9 +5,17 @@ class Solution {
      * @return Integer
      */
     function majorityElement($nums) {
+        $map = array();
         $count = array_count_values($nums);
-        $max = max($count);
-        $result = array_search($max , $count);
-        return $result;
+        print_r($count);
+        $max_ = max(array_values($count));
+        echo($max_);
+        foreach($count as $keys => $values){
+            if($values == $max_){
+                return $keys;
+            }
+        }
+        print_r(array_keys($count));
+        
     }
 }
