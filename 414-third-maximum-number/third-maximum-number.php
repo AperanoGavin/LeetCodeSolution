9ef1b0261ff5c6ed778f54d7c086c232e5a3ef65
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2024-11-13 20:28:08 +0100

    Time: 12 ms (24.24%) | Memory: 21.2 MB (9.09%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.php
+++ 414-third-maximum-number/third-maximum-number.php
@@ -5,24 +5,12 @@ class Solution {
      * @return Integer
      */
     function thirdMax($nums) {
-       // sort($nums);
-        $count = array_keys(array_count_values($nums));
-        sort($count);
-
-        $i = 0;
-        if(count(array_keys(array_count_values($count)))<3){
-            return ( max($nums));
+        $dic = array_unique($nums);
+        rsort($dic);
+        if(sizeof($dic)<3){
+            return max($dic);
         }
-        while ($i <= count($count) - 1 && $i < 2) {
-            array_pop($count);
-            $i++;
-        }
-
-        if (empty($count)) {
-            return max($nums);
-        } else {
-            return end($count);
-        }
-
+        return $dic[2];
+        
     }
 }
