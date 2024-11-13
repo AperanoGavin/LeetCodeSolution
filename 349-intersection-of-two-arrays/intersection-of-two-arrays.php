77644590eb7d7a1a6bc739ad09569c3dbf2e01ae
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2024-11-13 19:43:36 +0100

    Time: 5 ms (22.73%) | Memory: 20.2 MB (7.14%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.php
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.php
@@ -6,24 +6,16 @@ class Solution {
      * @return Integer[]
      */
     function intersection($nums1, $nums2) {
-        $unique_nums1 = array_unique($nums1);
-        $unique_nums2 = array_unique($nums2);
-
-        $map = [];
-
-       /*  for($i =0 ,$j=0; $i<=count($unique_nums1)-1 , $j<=count($unique_nums2)-1 ; $i++ , $j++ ){
-            if($unique_nums1[$i] == $unique_nums2[$j]){
-                array_push( $map , $unique_nums1[$i]);
-            }
-        } */
-
-        foreach ($unique_nums1 as $value1){
-            foreach ($unique_nums2 as $value2){
-                if( $value1 == $value2){
-                    array_push($map , $value1);
+        $map = array();
+        
+        /* for($i=0; $i<sizeof($nums1); $i++){
+            for($j=0; $j<sizeof($nums2); $j++){
+                if(($nums[$i] == $nums[$j] )){
+                    array_push($map , $nums[$j]);
                 }
             }
-        }
-        return $map;
+        } */
+        return array_unique(array_intersect($nums1 , $nums2));
+        
     }
 }
