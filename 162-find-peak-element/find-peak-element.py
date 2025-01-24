69f091e2b19dd9feedbbeeaf8f999c2a95bf72cb
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2025-01-24 01:38:13 +0100

    Time: 0 ms (100.00%) | Memory: 18.1 MB (11.70%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        #dic = []
+        nm = 0
+        j = max(nums)
+        return nums.index(j)
+            
+
+        
