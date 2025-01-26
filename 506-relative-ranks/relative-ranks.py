8ce64c5df285da748613f741836a87087003436a
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2025-01-26 18:40:43 +0100

    Time: 899 ms (5.12%) | Memory: 18.8 MB (64.41%) - LeetSync

--- 506-relative-ranks/relative-ranks.py
+++ 506-relative-ranks/relative-ranks.py
@@ -2,29 +2,23 @@ class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
         dic = score.copy()
         dic.sort(reverse=True)
-        """  mx = []
-        tmp = 0
-        while tmp < 3:
-            m = max(dic)
-            mx.append(m)
-            dic.remove(m)
-            tmp = tmp + 1
-        mx.sort()
-        print(mx)
-        print(score) """
         print(dic)
-
         for i in range(len(score)):
-            if   dic.index(score[i]) == 2:
-                    score[i] = "Bronze Medal"
-            elif   dic.index(score[i]) == 1:
-                    score[i] = "Silver Medal"
-            elif   dic.index(score[i]) == 0:
-                    score[i] = "Gold Medal"
-            else:
-                rs = dic.index(score[i]) +1
-                score[i] = f"{rs}"
+            if  dic.index(score[i]) == 0:
+                score[i] = "Gold Medal"
 
+            elif  dic.index(score[i]) == 1:
+                score[i] = "Silver Medal"
+
+            elif  dic.index(score[i]) == 2:
+                score[i] = "Bronze Medal"
+            
+            else :
+                num = dic.index(score[i]) +1
+                score[i] = f"{num}"
+        
         return score
+
+
 
       
